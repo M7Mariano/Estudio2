ebf0dde7f1edd5c2eff69d3626fb5676221e50c3
Heitor Gandolfi <heitor.gandolfi@hotmail.com>
2022-12-14 20:46:13 -0300

    Update HTML/Js
    Adicionando funcionalidades no formulário, a fim de limpar os campos após ser submetido.

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,10 @@
 const mobileBtn = document.querySelector(".mobile-btn");
 const closeBtn = document.querySelector(".close-menu");
+const formBtn = document.querySelector(".form-btn");
+const inputName = document.querySelector("#inputName");
+const inputEmail = document.querySelector("#inputEmail");
+const inputTel = document.querySelector("#inputTel");
+const textArea = document.querySelector("#textArea");
 
 // Funções
 
@@ -13,8 +18,16 @@ function closeMenu() {
     nav.classList.toggle("active");
 }
 
+function Submit() {
+    formBtn.preventDefault();
+    inputName.innerText = "";
+    inputEmail.innerText = "";
+    inputTel.innerText = "";
+    textArea.innerText = "";
+}
+
 // Eventos
 
 mobileBtn.addEventListener("click", toggleMenuMobile);
-
 closeBtn.addEventListener("click", closeMenu);
+formBtn.addEventListener("click", Submit);
